05741337de7020968515c4d20f3b3eeb86294346
Ali Rizvi <imsaar@gmail.com>
2011-04-21 07:25:22 -0700

    Initial Commit

---
+++ ViewControllerPractice/ViewControllerPracticeAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ViewControllerPracticeAppDelegate.h
+//  ViewControllerPractice
+//
+//  Created by Ali Rizvi on 4/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerPracticeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
